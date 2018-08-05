f83a51ec3bca7564277be529117420bf921deec7
Ann <annhromadjuk@gmail.com>
2018-08-05 13:11:47 +0300

    navigation nav-tabs

--- js/about.js
+++ js/about.js
@@ -1,17 +1,16 @@
+
 function validateName() {
-
     var name = document.getElementById('contact-name').value;
+    if(name.length == 0) {
 
-    if (name.length == 0) {
-
-        producePrompt("Вкажіть ім'я.", 'name-error', 'red')
+        producePrompt("Вкажіть ім'я.", 'name-error' , 'red');
         return false;
 
     }
 
-    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    if (name.length > 15) {
 
-        producePrompt("Прізвище та ім'я.", 'name-error', 'red');
+        producePrompt('макс. 15 символів','name-error', 'red');
         return false;
 
     }
@@ -25,38 +24,34 @@ function validatePhone() {
 
     var phone = document.getElementById('contact-phone').value;
 
-    if (phone.length == 0) {
+    if(phone.length == 0) {
         producePrompt('Введіть номер.', 'phone-error', 'red');
         return false;
     }
 
-    if (phone.length != 10) {
-        producePrompt('Введіть 10 цифр.', 'phone-error', 'red');
-        return false;
-    }
 
-    if (!phone.match(/^[0-9]{10}$/)) {
-        producePrompt('Тільки цифри.', 'phone-error', 'red');
+    if(!phone.match(/^[0-9]{12}$/)) {
+        producePrompt('Тільки цифри.' ,'phone-error', 'red');
         return false;
     }
 
     producePrompt('Дійсний', 'phone-error', 'green');
     return true;
-
 }
 
-function validateEmail() {
+function validateEmail () {
 
     var email = document.getElementById('contact-email').value;
+    var emailValidation = /^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 
-    if (email.length == 0) {
+    if(email.length == 0) {
 
-        producePrompt('Введіть email.', 'email-error', 'red');
+        producePrompt('Введіть email.','email-error', 'red');
         return false;
 
     }
 
-    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    if(!email.match(emailValidation)) {
 
         producePrompt('Email недійсний.', 'email-error', 'red');
         return false;
@@ -68,13 +63,18 @@ function validateEmail() {
 
 }
 
-function validateForm() {
+
+function validateForm(event) {
+    event.preventDefault();
+    console.log('form is sent', event);
     if (!validateName() || !validatePhone() || !validateEmail()) {
         jsShow('submit-error');
-        producePrompt('Виправте помилки,щоб відправити.', 'submit-error', 'red');
-        setTimeout(function() { jsHide('submit-error'); }, 2000);
+        producePrompt('Виправте помилки, щоб відправити.', 'submit-error', 'red');
+        setTimeout(function(){jsHide('submit-error');}, 2000);
         return false;
+
     }
+
 }
 
 function jsShow(id) {
@@ -89,6 +89,5 @@ function producePrompt(message, promptLocation, color) {
 
     document.getElementById(promptLocation).innerHTML = message;
     document.getElementById(promptLocation).style.color = color;
-
 
 }
